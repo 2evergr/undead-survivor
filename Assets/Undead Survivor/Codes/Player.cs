2d419a8b5f69b342cfef004d2eff90a228519401
김진환 <jinhwan.kim@spendit.kr>
2024-01-21 11:33:44 +0900

    change script name

--- Assets/Undead Survivor/Codes/Player.cs
+++ Assets/Undead Survivor/Codes/Player.cs
@@ -4,62 +4,64 @@ using Unity.Android.Gradle;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Player : MonoBehaviour
 {
-    [SerializeField]
-    Vector2 inputVec;
-    Rigidbody2D rigid;
-    SpriteRenderer spriter;
-    Animator anim;
+  [SerializeField]
+  Vector2 inputVec;
+  Rigidbody2D rigid;
+  SpriteRenderer spriter;
+  Animator anim;
 
-    [SerializeField]
-    float speed;
+  [SerializeField]
+  float speed;
 
-    void Awake()
-    {
-       rigid = GetComponent<Rigidbody2D>();
-       spriter = GetComponent<SpriteRenderer>();
-       anim = GetComponent<Animator>();
+  void Awake()
+  {
+    rigid = GetComponent<Rigidbody2D>();
+    spriter = GetComponent<SpriteRenderer>();
+    anim = GetComponent<Animator>();
+  }
+
+  // Start is called before the first frame update
+  void Start()
+  {
+      
+  }
+    
+  // Update is called once per frame
+  // void Update()
+  // {
+  //     inputVec.x = Input.GetAxisRaw("Horizontal");
+  //     inputVec.y = Input.GetAxisRaw("Vertical");
+
+  //     if (inputVec.x < 0) {
+  //         sprite.flipX = true;
+  //     } else if (inputVec.x > 0) {
+  //         sprite.flipX = false;
+  //     }
+  // }
+
+  void FixedUpdate()
+  {
+      // rigid.AddForce(inputVec);
+      // rigid.velocity = inputVec;
+      Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
+      rigid.MovePosition(rigid.position + nextVec);
+  }
+
+  void OnMove(InputValue value)
+  {
+    inputVec = value.Get<Vector2>();
+  }
+
+  // LateUpdate 는 update 가 끝나고 다음 frame 이 시작되기 직전에 실행됨
+  void LateUpdate()
+  {
+    anim.SetFloat("Speed", inputVec.magnitude);
+
+    // 사용자의 입력이 있을 경우 값에 따라 방향 변경
+    if (inputVec.x != 0) {
+        spriter.flipX = inputVec.x < 0;
     }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    // void Update()
-    // {
-    //     inputVec.x = Input.GetAxisRaw("Horizontal");
-    //     inputVec.y = Input.GetAxisRaw("Vertical");
-
-    //     if (inputVec.x < 0) {
-    //         sprite.flipX = true;
-    //     } else if (inputVec.x > 0) {
-    //         sprite.flipX = false;
-    //     }
-    // }
-
-    void FixedUpdate()
-    {
-        // rigid.AddForce(inputVec);
-        // rigid.velocity = inputVec;
-        Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
-        rigid.MovePosition(rigid.position + nextVec);
-    }
-
-    void OnMove(InputValue value)
-    {
-        inputVec = value.Get<Vector2>();
-    }
-
-    void LateUpdate()
-    {
-        anim.SetFloat("Speed", inputVec.magnitude);
-
-        if (inputVec.x != 0) {
-            spriter.flipX = inputVec.x < 0;
-        }
-    }
+  }
 }
