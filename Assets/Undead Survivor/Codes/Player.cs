2e356370a8690aa5a2580c3b024833a7dbf7b46d
김진환 <jinhwan.kim@spendit.kr>
2024-01-27 11:56:15 +0900

    무한맵이동 구현 static GameManager 구현 Cinemachine package 추가 & virtual camera 적용

--- Assets/Undead Survivor/Codes/Player.cs
+++ Assets/Undead Survivor/Codes/Player.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Android.Gradle;
 using UnityEngine;
+// 새로운 InputSystem 적용을 위해 추가
 using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-  [SerializeField]
-  Vector2 inputVec;
+  public Vector2 inputVec;
   Rigidbody2D rigid;
   SpriteRenderer spriter;
   Animator anim;
@@ -33,12 +33,6 @@ public class Player : MonoBehaviour
   // {
   //     inputVec.x = Input.GetAxisRaw("Horizontal");
   //     inputVec.y = Input.GetAxisRaw("Vertical");
-
-  //     if (inputVec.x < 0) {
-  //         sprite.flipX = true;
-  //     } else if (inputVec.x > 0) {
-  //         sprite.flipX = false;
-  //     }
   // }
 
   void FixedUpdate()
@@ -49,6 +43,7 @@ public class Player : MonoBehaviour
       rigid.MovePosition(rigid.position + nextVec);
   }
 
+  // 새로운 inputSystem 에서 제공하는 event
   void OnMove(InputValue value)
   {
     inputVec = value.Get<Vector2>();
@@ -57,6 +52,7 @@ public class Player : MonoBehaviour
   // LateUpdate 는 update 가 끝나고 다음 frame 이 시작되기 직전에 실행됨
   void LateUpdate()
   {
+    // 이력값의 크기만 보고 싶을때 magnitude
     anim.SetFloat("Speed", inputVec.magnitude);
 
     // 사용자의 입력이 있을 경우 값에 따라 방향 변경
